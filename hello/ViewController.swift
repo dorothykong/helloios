562f26cbd8e6372e8ebce255f7d213d44b7526e1
Dorothy Kong <dorothykong@D-108-179-166-205.dhcp4.washington.edu>
2015-10-01 17:40:40 -0700

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Dorothy Kong on 10/1/15.
+//  Copyright (c) 2015 Dorothy Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
